d2992b726a3f41f325efb718955087f8cd48b287
harshkc03 <harshkc2000@gmail.com>
2021-02-24 16:23:02 +0530

    js code typo fixed

--- js/script_map.js
+++ js/script_map.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 });
 
 function refreshItems() {
-    var url = "https://spreadsheets.google.com/feeds/list/1gqAmKsjCGLHpCGi9Id-vDyNTD0KJ7OWlyGCshMgIk80/5/public/full?alt=json"
+    var url = "https://spreadsheets.google.com/feeds/list/1gqAmKsjCGLHpCGi9Id-vDyNTD0KJ7OWlyGCshMgIk80/5/public/full?alt=json";
     var graph_arr = [
         ['Order ID', 'Time Taken', {
             role: 'style'
@@ -55,103 +55,102 @@ function refreshItems() {
 
 
         var trHTML = '';
-        if (previousLength != data.feed.entry.length || flag == true) {
 
-            previousLength = data.feed.entry.length;
-            previousValue = currentValue;
-            console.log(previousValue)
+        previousLength = data.feed.entry.length;
+        previousValue = currentValue;
+        console.log(previousValue)
 
-            //table
-            for (var i = 0; i < data.feed.entry.length; ++i) {
-                var myData_map, myData_order;
+        //table
+        for (var i = 0; i < data.feed.entry.length; ++i) {
+            var myData_map, myData_order;
 
-                var json_data2 = {
-                    "City": data.feed.entry[i].gsx$city.$t,
-                    "OderID": data.feed.entry[i].gsx$orderid.$t,
-                    "Item": data.feed.entry[i].gsx$item.$t,
-                    "Latitude": parseFloat(data.feed.entry[i].gsx$latitude.$t),
-                    "Longitude": parseFloat(data.feed.entry[i].gsx$longitude.$t),
-                    "Dispatched": data.feed.entry[i].gsx$dispatched.$t,
-                    "Shipped": data.feed.entry[i].gsx$shipped.$t
-                };
-                jsonMapObject.push(json_data2);
+            var json_data2 = {
+                "City": data.feed.entry[i].gsx$city.$t,
+                "OderID": data.feed.entry[i].gsx$orderid.$t,
+                "Item": data.feed.entry[i].gsx$item.$t,
+                "Latitude": parseFloat(data.feed.entry[i].gsx$latitude.$t),
+                "Longitude": parseFloat(data.feed.entry[i].gsx$longitude.$t),
+                "Dispatched": data.feed.entry[i].gsx$dispatched.$t,
+                "Shipped": data.feed.entry[i].gsx$shipped.$t
+            };
+            jsonMapObject.push(json_data2);
 
-            }
+        }
 
-            for (var j = 0; j < jsonMapObject.length; j++) {
+        for (var j = 0; j < jsonMapObject.length; j++) {
 
-                var colorIcon;
+            var colorIcon;
 
-                var LeafIcon = L.Icon.extend({
-                    options: {
+            var LeafIcon = L.Icon.extend({
+                options: {
 
-                        iconAnchor: [20, 30]
-                    }
+                    iconAnchor: [20, 30]
+                }
+            });
+
+            var greenIcon = new LeafIcon({
+                iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png'
+            }),
+                redIcon = new LeafIcon({
+                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png'
+                }),
+                yellowIcon = new LeafIcon({
+                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png'
                 });
 
-                var greenIcon = new LeafIcon({
-                    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png'
-                }),
-                    redIcon = new LeafIcon({
-                        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png'
-                    }),
-                    yellowIcon = new LeafIcon({
-                        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-gold.png'
-                    });
 
 
-
-                if (jsonMapObject[j].Dispatched == "YES" && jsonMapObject[j].Shipped == "YES") {
-                    colorIcon = greenIcon;
-                } else if (jsonMapObject[j].Dispatched == "YES" && jsonMapObject[j].Shipped != "YES") {
-                    colorIcon = yellowIcon;
-                } else if (jsonMapObject[j].Dispatched != "YES" && jsonMapObject[j].Dispatched != "YES") {
-                    colorIcon = redIcon;
-                }
-                var marker = L.marker(L.latLng(parseFloat(jsonMapObject[j].Latitude), parseFloat(jsonMapObject[j].Longitude)), {
-                    icon: colorIcon
-                }).addTo(map);
-
-                marker.on('click', onClick_Marker)
-                marker.on('mousehover')
-                // Attach the corresponding JSON data to your marker:
-                marker.myJsonData = jsonMapObject[j];
-                console.log(marker.myJsonData)
-
-                function onClick_Marker(e) {
-                    var marker = e.target;
-                    popup = L.popup()
-                        .setLatLng(marker.getLatLng())
-                        // .setContent("Order ID: " + marker.myJsonData.OderID + " || Item: " +
-                        //     marker.myJsonData.Item)
-                        .setContent(
-                            "<center>" +
-                            "<h2><b>" +
-                            marker.myJsonData.City +
-                            "</b></h2>" +
-                            "<b>Order ID:</b>  " +
-                            marker.myJsonData.OderID +
-                            ", <b>Item:</b> " +
-                            marker.myJsonData.Item +
-                            "<br />" +
-                            " <b>Dispatched:</b> " +
-                            marker.myJsonData.Dispatched +
-                            ", <b>Shipped:</b> " +
-                            marker.myJsonData.Shipped +
-                            "</center>"
-                        )
-                        .openOn(map);
-                }
-
-                L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                }).addTo(map);
-
-
+            if (jsonMapObject[j].Dispatched == "YES" && jsonMapObject[j].Shipped == "YES") {
+                colorIcon = greenIcon;
+            } else if (jsonMapObject[j].Dispatched == "YES" && jsonMapObject[j].Shipped != "YES") {
+                colorIcon = yellowIcon;
+            } else if (jsonMapObject[j].Dispatched != "YES" && jsonMapObject[j].Dispatched != "YES") {
+                colorIcon = redIcon;
             }
+            var marker = L.marker(L.latLng(parseFloat(jsonMapObject[j].Latitude), parseFloat(jsonMapObject[j].Longitude)), {
+                icon: colorIcon
+            }).addTo(map);
+
+            marker.on('click', onClick_Marker)
+            marker.on('mousehover')
+            // Attach the corresponding JSON data to your marker:
+            marker.myJsonData = jsonMapObject[j];
+            console.log(marker.myJsonData)
+
+            function onClick_Marker(e) {
+                var marker = e.target;
+                popup = L.popup()
+                    .setLatLng(marker.getLatLng())
+                    // .setContent("Order ID: " + marker.myJsonData.OderID + " || Item: " +
+                    //     marker.myJsonData.Item)
+                    .setContent(
+                        "<center>" +
+                        "<h2><b>" +
+                        marker.myJsonData.City +
+                        "</b></h2>" +
+                        "<b>Order ID:</b>  " +
+                        marker.myJsonData.OderID +
+                        ", <b>Item:</b> " +
+                        marker.myJsonData.Item +
+                        "<br />" +
+                        " <b>Dispatched:</b> " +
+                        marker.myJsonData.Dispatched +
+                        ", <b>Shipped:</b> " +
+                        marker.myJsonData.Shipped +
+                        "</center>"
+                    )
+                    .openOn(map);
+            }
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
 
 
         }
+
+
+        
 
 
     });
